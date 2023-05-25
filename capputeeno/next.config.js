3dada13cccc5d3423ecbc9bf749f625464e0e952
dpfl-dev <duarte.f.p.l@gmail.com>
2023-05-25 12:25:05 +0100

    cleaning up

--- capputeeno/next.config.js
+++ capputeeno/next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
     compiler: {
         styledComponents: true
     }
 }
-=======
-const nextConfig = {}
->>>>>>> 72bcd24ea19e3ec97fc4f106fd793e832a051078
 
 module.exports = nextConfig
